0e6b65bf40e5dbfbce3f25680731212ac5d096e9
GauravKumar1502 <gauravkumar15021999@gmail.com>
2024-05-12 11:31:18 +0530

    feat: Add new components for home, about, contact, profile, and pagenot-found
    This commit adds new components for the home, about, contact, profile, and pagenot-found pages. Each component includes an HTML template and a TypeScript file. These components are necessary for building the different pages of the application.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { PagenotFoundComponent } from './components/pagenot-found/pagenot-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: 'profile', component: ProfileComponent},
+    {path: '**', component: PagenotFoundComponent}
+];
